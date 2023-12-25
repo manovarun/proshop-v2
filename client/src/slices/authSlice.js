a61b226931aaf3c25dcf61a22a38fc06e9ff58a7
manovarun <samvarun@gmail.com>
2023-12-25 14:03:22 +0530

    Auth Slice Created

--- client/src/slices/authSlice.js
+++ client/src/slices/authSlice.js
@@ -5,3 +5,18 @@ const initialState = {
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
 };
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload.user;
+      localStorage.setItem('userInfo', JSON.stringify(action.payload.user));
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
